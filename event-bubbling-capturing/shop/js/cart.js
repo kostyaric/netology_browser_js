c2192b54c2a86ef725fd67a2779df712be7e9c2a
kost <kost@domnet.org>
2019-06-23 17:38:20 +0300

    Принцип обработки событий

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,15 @@
 'use strict';
+let itemList = document.querySelector('.items-list');
+itemList.addEventListener('click', onClick);
+
+function onClick(event) {
+
+  event.preventDefault();
+
+  if (event.target.classList.contains('add-to-cart')) {
+  
+  	addToCart(event.target.dataset);
+
+  }
+
+}
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -2,4 +2,16 @@
 
 function handleTableClick(event) {
   
+  if (event.target.classList.contains('prop__name')) {
+
+    if (event.target.dataset.dir == 1) {
+      event.target.dataset.dir = -1;
+    } else {
+      event.target.dataset.dir = 1;
+    }
+
+    event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+    sortTable(event.target.dataset.propName, Number(event.target.dataset.dir));
+  }
+
 }
